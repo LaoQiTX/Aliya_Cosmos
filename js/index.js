939b8752ca9132629c4ab0350bf1b271dff5c7ea
zwb <695683346@qq.com>
2025-04-24 16:18:09 +0800

    update 1.更新心率判断bug

--- js/index.js
+++ js/index.js
@@ -49,7 +49,7 @@ function updateHeartRate(min,max) {
 	const bpmElement = document.querySelector('.bpm');
 	// debugger;
 	const randomHeartRate = getRandomHeartRate(min, max); // 设置心率范围
-	if (min>0 || max>0) {
+	if (min > max && min > 0 ) {
 		document.querySelector('.heart-rate').style.backgroundImage = "none";	
 		// document.querySelector('#ecgCanvas').style.display = "block";
 	}
