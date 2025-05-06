a80c07ed3849186cbc93ec4e894a87fdc3cb987f
zwb <695683346@qq.com>
2025-05-06 17:06:10 +0800

    update 1.更新变量

--- js/index.js
+++ js/index.js
@@ -776,15 +776,15 @@ function loadCacheData(dialogueDto, cacheOptionList, message, currentMessageInde
  * @param {JSON} message 剧情文本
  */
 function sendMsg(content, message) {
-	var isLoad = content != null;
+	var isLoad = content == null;
 	if (message.image_url) {
-		addImageMessage(message.image_url, false, !isLoad);
+		addImageMessage(message.image_url, false, isLoad);
 	} else {
 		// 如果aliya发送的消息不是img的时候 则需要把他的content更新到content进行输出
 		if (message.type == 'aliya') {
 			content = message.content[0];
 		}
-		addMessage(content, message.type !== 'aliya', !isLoad);
+		addMessage(content, message.type !== 'aliya', isLoad);
 	}
 	if (isLoad == false) {
 		// debugger;
